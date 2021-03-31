827fc14f8b2a0bc43b89488a7e5c2621f5aec72c
charlotte@virtualtrips.io <char502@hotmail.com>
2021-03-31 17:56:07 +0100

    added events pages and header

--- dummy-data.js
+++ dummy-data.js
@@ -13,7 +13,7 @@ const DUMMY_EVENTS = [
     id: 'e2',
     title: 'Networking for introverts',
     description:
-      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promise!",
     location: 'New Wall Street 5, 98765 New Work',
     date: '2021-05-30',
     image: 'images/introvert-event.jpg',
@@ -23,7 +23,7 @@ const DUMMY_EVENTS = [
     id: 'e3',
     title: 'Networking for extroverts',
     description:
-      'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
+      'You probably don\'t need help with networking in general. But focusing your energy correctly - that is something most people can improve.',
     location: 'My Street 12, 10115 Broke City',
     date: '2022-04-10',
     image: 'images/extrovert-event.jpg',
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,4 +1,12 @@
+
+import { getAllEvents } from '../../dummy-data'
+
 function AllEventsPage() {
+
+  const allEvents = getAllEvents()
+
+  console.log(allEvents)
+
   return (
     <div>
       <h1>All Events</h1>
